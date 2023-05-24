3c7f665f002500496cc89c7400bf328ba71355b5
shariar17e <shariarhossain23@gmail.com>
2023-05-24 22:42:34 +0600

    swap done

---
+++ practice-problem/practice-problem-1/calculator.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    long long int x;
+    long long int y;
+    
+    cin>>x>>y;
+
+    long long int sum = x+y;
+    long long int mul = x*y;
+    long long int sub = x-y;
+
+    cout<<x<<" "<<"+"<<" "<<y<<" "<<"="<<" "<<sum<<endl;
+    cout<<x<<" "<<"*"<<" "<<y<<" "<<"="<<" "<<mul<<endl;
+    cout<<x<<" "<<"-"<<" "<<y<<" "<<"="<<" "<<sub<<endl;
+
+    return 0;
+}
